e07cad76934b6faa9090d4556c7627e2bc01a4da
satomi kenta <satomikennta1213@gmail.com>
2021-07-04 23:37:50 +0900

    initial commit

--- lib/stepfn-cdk-stack.ts
+++ lib/stepfn-cdk-stack.ts
@@ -1,9 +1,42 @@
 import * as cdk from '@aws-cdk/core';
+import * as StepFn from '@aws-cdk/aws-stepfunctions';
+import * as StepFnTask from '@aws-cdk/aws-stepfunctions-tasks';
+import * as Logs from '@aws-cdk/aws-logs';
+import { LogLevel, StateMachineType } from '@aws-cdk/aws-stepfunctions';
+import { CfnOutput, Duration } from '@aws-cdk/core';
 
 export class StepfnCdkStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    const activity = new StepFn.Activity(this, "DemoActivityFromCDK", { activityName: "DemoActivityFromCDK" });
+    
+    const workflow = new StepFnTask.StepFunctionsInvokeActivity(this, "StepFnInvokeActivity", {
+      activity: activity,
+    });
+    // ここにchain して workflowを構成していく 
+
+    const logGroup = new Logs.LogGroup(this, 'MyLogGroup');
+
+    const stateMachine = new StepFn.StateMachine(this, "DemoStateMachineFromCDK", {
+      stateMachineName: "DemoStateMachineFromCDK",
+      definition: workflow,
+      stateMachineType: StateMachineType.STANDARD,
+      timeout: Duration.seconds(60),
+      logs: {
+        level: LogLevel.ALL,
+        destination: logGroup
+      }
+    });
+
+    new CfnOutput(this, "ActivityArnOutput", {
+      exportName: "activityArn",
+      value: activity.activityArn
+    });
+
+    new CfnOutput(this, "StateMachineArnOutput", {
+      exportName: "stateMachineArn",
+      value: stateMachine.stateMachineArn
+    });
   }
 }
