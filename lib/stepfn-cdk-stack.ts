8f5d4372244b64cc20e6625bbea3ee5fa88ea936
satomi kenta <satomikennta1213@gmail.com>
2021-07-05 15:43:40 +0900

    add: callback pattern: SNS email confirmation

--- lib/stepfn-cdk-stack.ts
+++ lib/stepfn-cdk-stack.ts
@@ -2,7 +2,10 @@ import * as cdk from '@aws-cdk/core';
 import * as StepFn from '@aws-cdk/aws-stepfunctions';
 import * as StepFnTask from '@aws-cdk/aws-stepfunctions-tasks';
 import * as Logs from '@aws-cdk/aws-logs';
-import { LogLevel, StateMachineType } from '@aws-cdk/aws-stepfunctions';
+import * as Lambda from '@aws-cdk/aws-lambda';
+import * as SNS from '@aws-cdk/aws-sns';
+import * as Subscription from '@aws-cdk/aws-sns-subscriptions';
+import { InputType, LogLevel, StateMachineType } from '@aws-cdk/aws-stepfunctions';
 import { CfnOutput, Duration } from '@aws-cdk/core';
 
 export class StepfnCdkStack extends cdk.Stack {
@@ -11,10 +14,45 @@ export class StepfnCdkStack extends cdk.Stack {
 
     const activity = new StepFn.Activity(this, "DemoActivityFromCDK", { activityName: "DemoActivityFromCDK" });
     
-    const workflow = new StepFnTask.StepFunctionsInvokeActivity(this, "StepFnInvokeActivity", {
+    const task = new StepFnTask.StepFunctionsInvokeActivity(this, "StepFnInvokeActivity", {
       activity: activity,
+      timeout: Duration.seconds(30),
     });
-    // ここにchain して workflowを構成していく 
+    const doubleTheNumber = new StepFnTask.EvaluateExpression(this, 'Double the number', {
+      expression: '$.number * 2',
+      resultPath: '$.number',
+      runtime: Lambda.Runtime.NODEJS_14_X,
+    });
+
+    
+    const myTopic = new SNS.Topic(this, 'MyTopic');
+    myTopic.addSubscription(new Subscription.EmailSubscription('satomikennta1213@gmail.com'));
+    
+    // Callback Pattern
+    const sendEmailTask = new StepFnTask.SnsPublish(this, 'SNSSendEmail', {
+      topic: myTopic,
+      message: {
+        type: InputType.OBJECT,
+        value: { literal: 'literal', token: StepFn.JsonPath.stringAt('$$.Task.Token') }
+      },
+      subject: "Confirmation Email",
+      integrationPattern: StepFn.IntegrationPattern.WAIT_FOR_TASK_TOKEN,
+      timeout: Duration.minutes(30)
+    });
+
+    const jobFailed = new StepFn.Fail(this, 'Job Failed', {
+      cause: 'Job Failed',
+      error: 'Job returned FAILED',
+    });
+    const jobSucceeded = new StepFn.Succeed(this, 'Job Succeeded');
+
+    const workflow = task
+    .next(sendEmailTask)
+    .next(doubleTheNumber)
+    .next(new StepFn.Choice(this, 'even number ?')
+      .when(StepFn.Condition.numberEquals('$.number', 4), jobSucceeded)
+      .when(StepFn.Condition.numberEquals('$.number', 5), jobFailed)
+    );
 
     const logGroup = new Logs.LogGroup(this, 'MyLogGroup');
 
@@ -22,7 +60,7 @@ export class StepfnCdkStack extends cdk.Stack {
       stateMachineName: "DemoStateMachineFromCDK",
       definition: workflow,
       stateMachineType: StateMachineType.STANDARD,
-      timeout: Duration.seconds(60),
+      timeout: Duration.seconds(90),
       logs: {
         level: LogLevel.ALL,
         destination: logGroup
